91f1cc8fa2170fcc254aab677d08ee0c2113af76
yuvmironov <yuvmironov@tehnosk.ru>
2022-03-11 18:44:40 +0300

    Fix + Up ver

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import yuvInput from '../components/yuv-input'
 import yuvButton from '../components/yuv-button'
 import yuvNotification from '../components/yuv-notification'
 import yuvDialog from '../components/yuv-dialog'
-import yuvGlobalLoader from '@/components/yuv-globalLoader'
+import yuvGlobalLoader from '../components/yuv-globalLoader'
 
 export default {
   yuvButton,
