f8174f7b05cd7ab1d84eb67b74e3d091d6074a54
yuvmironov <yuvmironov@tehnosk.ru>
2022-04-27 11:31:03 +0300

    Fix path

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import yuvInput from '../components/yuv-input'
 import yuvButton from '../components/yuv-button'
-import yuvIconButton from '@/components/yuv-icon-button'
+import yuvIconButton from '../components/yuv-icon-button'
 import yuvNotification from '../components/yuv-notification'
 import yuvDialog from '../components/yuv-dialog'
 import yuvGlobalLoader from '../components/yuv-globalLoader'
